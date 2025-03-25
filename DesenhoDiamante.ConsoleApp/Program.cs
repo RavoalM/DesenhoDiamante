bd839885c231304614a4b12b221e62fd96416425
Alvaro Machado <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-25 20:09:50 -0300

    Começo do processo de validação

--- DesenhoDiamante.ConsoleApp/Program.cs
+++ DesenhoDiamante.ConsoleApp/Program.cs
@@ -9,49 +9,49 @@
             int Subtracao = 0;
             int Soma = 0;
 
-
-            int EspacoSuperior = NumeroDiamante / 2;
-            int Espacos = EspacoSuperior;
-            int QuantidadeX = 1;
-            for (int i = EspacoSuperior; i <= NumeroDiamante; i++)
-            {
-               
-              if (QuantidadeX >= NumeroDiamante || Espacos < 0)
-              {
-                break;
-              }
-
-                Console.WriteLine(new string(' ', Espacos) + new string('X', QuantidadeX));
-                Soma += 2;
-                QuantidadeX = 1 + Soma;
-                Espacos -= 1;
-            }
-
-            Console.WriteLine(new string('X', NumeroDiamante));
-
-            Espacos = 0;
-            Subtracao = 0;
-           
-            for (int i = 0; i < NumeroDiamante; i++)
+            if (NumeroDiamante % 2 != 0 && NumeroDiamante > 1)
             {
 
-                Espacos += 1;
-                Subtracao += 2;
+                int EspacoSuperior = NumeroDiamante / 2;
+                int Espacos = EspacoSuperior;
+                int QuantidadeX = 1;
+                for (int i = EspacoSuperior; i <= NumeroDiamante; i++)
+                {
 
-                QuantidadeX = NumeroDiamante - Subtracao;
-                if (QuantidadeX < 0)
-                    break;
+                    if (QuantidadeX >= NumeroDiamante || Espacos < 0)
+                    {
+                        break;
+                    }
 
-                Console.WriteLine(new string(' ', Espacos) + new string('X', QuantidadeX));
+                    Console.WriteLine(new string(' ', Espacos) + new string('X', QuantidadeX));
+                    Soma += 2;
+                    QuantidadeX = 1 + Soma;
+                    Espacos -= 1;
+                }
+
+                Console.WriteLine(new string('X', NumeroDiamante));
+
+                Espacos = 0;
+                Subtracao = 0;
+
+                for (int i = 0; i < NumeroDiamante; i++)
+                {
+
+                    Espacos += 1;
+                    Subtracao += 2;
+
+                    QuantidadeX = NumeroDiamante - Subtracao;
+                    if (QuantidadeX < 0)
+                        break;
+
+                    Console.WriteLine(new string(' ', Espacos) + new string('X', QuantidadeX));
+                }
             }
-
-
-
-            //Console.WriteLine(new string(' ', 2) + new string('X', NumeroDiamante - 4));
-            //Console.WriteLine(new string(' ', 1) + new string('X', NumeroDiamante - 2));
-            //Console.WriteLine(new string('X', NumeroDiamante));
-            //Console.WriteLine(new string(' ', 1) + new string('X', NumeroDiamante - 2));
-            //Console.WriteLine(new string(' ', 2) + new string('X', NumeroDiamante - 4));
+            else
+            {
+                Console.WriteLine("Número inválido. Digite um número ímpar e maior que 1.");
+            }
         }
+
     }
 }
