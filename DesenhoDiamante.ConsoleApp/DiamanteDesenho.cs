a7f6be7513f287caea1a854124019184a1d976aa
Alvaro Machado <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-26 00:39:33 -0300

    Função desenho meio implementada

---
+++ DesenhoDiamante.ConsoleApp/DiamanteDesenho.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesenhoDiamante.ConsoleApp
+{
+    public class DiamanteDesenho
+    {
+        public string ParteCima(int NumeroDiamante)
+        {
+            int Soma = 0;
+            StringBuilder Diamante = new StringBuilder();
+            int EspacoSuperior = NumeroDiamante / 2;
+            int Espacos = EspacoSuperior;
+            int QuantidadeX = 1;
+            for (int i = EspacoSuperior; i <= NumeroDiamante; i++)
+            {
+                if (QuantidadeX >= NumeroDiamante || Espacos < 0)
+                {
+                    break;
+                }
+
+                Diamante.AppendLine(new string(' ', Espacos) + new string('X', QuantidadeX));
+                Soma += 2;
+                QuantidadeX = 1 + Soma;
+                Espacos -= 1;
+            }
+            return Diamante.ToString();
+        }
+        public static void ParteMeio(int NumeroDiamante)
+        {
+            Console.WriteLine(new string('X', NumeroDiamante));
+        }
+    }
+}
--- DesenhoDiamante.ConsoleApp/Program.cs
+++ DesenhoDiamante.ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        static int Espacos = 0;
+        static int Subtracao = 0;
         static void Main()
         {
             while (true)
@@ -14,35 +16,13 @@
                     continue;
                 }
 
-                int Subtracao = 0;
-                int Soma = 0;
-
 
                 if (NumeroDiamante % 2 != 0 && NumeroDiamante > 1)
                 {
+                    string ParteCima = new DiamanteDesenho().ParteCima(NumeroDiamante).TrimEnd();
+                    Console.WriteLine(ParteCima);
                     
-                    int EspacoSuperior = NumeroDiamante / 2;
-                    int Espacos = EspacoSuperior;
-                    int QuantidadeX = 1;
-                    Console.Clear();
-                    for (int i = EspacoSuperior; i <= NumeroDiamante; i++)
-                    {
-
-                        if (QuantidadeX >= NumeroDiamante || Espacos < 0)
-                        {
-                            break;
-                        }
-
-                        Console.WriteLine(new string(' ', Espacos) + new string('X', QuantidadeX));
-                        Soma += 2;
-                        QuantidadeX = 1 + Soma;
-                        Espacos -= 1;
-                    }
-
-                    Console.WriteLine(new string('X', NumeroDiamante));
-
-                    Espacos = 0;
-                    Subtracao = 0;
+                    DiamanteDesenho.ParteMeio(NumeroDiamante);
 
                     for (int i = 0; i < NumeroDiamante; i++)
                     {
@@ -50,7 +30,7 @@
                         Espacos += 1;
                         Subtracao += 2;
 
-                        QuantidadeX = NumeroDiamante - Subtracao;
+                        int QuantidadeX = NumeroDiamante - Subtracao;
                         if (QuantidadeX < 0)
                             break;
 
